9417d5fca36dc37fcebd3ef27e85615a89a6d3a4
GABS115 <gabriel.sirino115@gmail.com>
2024-05-14 21:45:34 -0300

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -12,12 +12,33 @@ function savePost()
    
 }
 
+function updatePost()
+{
+    const bodyPost =  getBodyPost()
+
+    fetch('https://jsonplaceholder.typicode.com/posts/101',{
+        method : "PUT",
+        body : bodyPost
+    })
+    .then(data => data.json())
+    .then(response => console.log(response))
+}
+
+function deletePost()
+{
+    fetch('https://jsonplaceholder.typicode.com/posts/101',{
+        method : "DELETE",
+    })
+    .then(data => data.json())
+    .then(response => console.log(response))
+}
+
 function getBodyPost()
 {
     const inputTitle = document.getElementById("input-title").value
     const inputBody = document.getElementById("input-body").value
     const inputUserId = document.getElementById("input-user-id").value
-    
+
     const body = JSON.stringify ({
         userId : inputUserId,
         title: inputTitle,
